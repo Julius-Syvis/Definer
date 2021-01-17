1854e8176aae5de6512b2517bd97d043087d0bcc
Julius-Syvis <55212122+Julius-Syvis@users.noreply.github.com>
2021-01-17 13:19:27 +0200

    docs: add coments for data fetching

--- src/js/define.js
+++ src/js/define.js
@@ -11,23 +11,27 @@ function define(phrase) {
 // The action of saving a phrase to private dictionary
 function save(phrase) {
   if (phrase != null) {
+    // Get stored flag 'record' from memory
+    // If it's enabled, save new entry to history
     chrome.storage.local.get(["record"], function (recordResult) {
       recordChecked = recordResult.record;
       if (recordChecked == true) {
+        // Get stored history from memory
         chrome.storage.local.get(["history"], function (historyResult) {
           arr = historyResult.history;
           if (arr == undefined || !Array.isArray(arr)) {
             arr = [];
           }
+          // Get stored flag 'duplicate' from memory
+          // If it's enabled, allow duplicate entries
           chrome.storage.local.get(["duplicate"], function (duplicateResult) {
             duplicateChecked = duplicateResult.duplicate;
             if (duplicateChecked) {
               arr.push(phrase);
-            } else {
-              if (!arr.includes(phrase)) {
-                arr.push(phrase);
-              }
+            } else if (!arr.includes(phrase)) {
+              arr.push(phrase);
             }
+
             if (arr.length > 20) {
               arr.shift();
             }
